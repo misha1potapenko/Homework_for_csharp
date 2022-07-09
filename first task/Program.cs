092e7243e55de18bdf77052dc44bd7054f8dd433
misha1potapenko <misha1potapenko@yandex.ru>
2022-07-09 10:53:19 +0300

    added first homework

--- first task/Program.cs
+++ first task/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.Write("Напишите число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Напишите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if ( a > b)
+{
+    Console.Write("Больше число a = ");
+    Console.WriteLine(a);
+}
+else 
+{
+    Console.Write("Больше число b = ");
+    Console.WriteLine(b);
+}
